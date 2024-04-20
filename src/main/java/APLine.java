6b6529fab1852aee1d7de43eaff01306cf7aefc2
leighlawnee <143223104+leighlawnee@users.noreply.github.com>
2024-04-20 14:04:11 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,14 @@
-public class APLine
-{
-  /* your code here */
+public class APLine{
+private int a;
+private int b;
+private int c;
+
+public double getSlope(){
+return -a/(double)b;
+}
   
+public boolean isOnLine(int x, int y){
+return ax + by + c ==0;
+}
+
 }
